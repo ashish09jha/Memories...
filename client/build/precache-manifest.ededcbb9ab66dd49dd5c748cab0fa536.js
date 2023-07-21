9802f4dda81ec0a30cbeed7548919ebe4ea23b44
ashish09jha <ashishjha7213@gmail.com>
2023-07-21 19:44:55 +0530

    memories initial

---
+++ client/build/precache-manifest.ededcbb9ab66dd49dd5c748cab0fa536.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e79a83e934cd65be347d9866b613954",
+    "url": "/index.html"
+  },
+  {
+    "revision": "067843bda419f35f17d2",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "ea8c5c5b4fc82071a83c",
+    "url": "/static/js/2.ab77b817.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.ab77b817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067843bda419f35f17d2",
+    "url": "/static/js/main.2f7de305.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://ashishjha7213:Ashishjha9560@cluster0.kmvatkn.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://ashishjha7213:Ashishjha7213@cluster0.s4pnz0w.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
